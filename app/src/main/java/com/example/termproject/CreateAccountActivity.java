b52a008969e09f7421ba0dfa516e5a288d1e56cb
Ben Primeau <ben.r.primeau@gmail.com>
2021-10-21 22:01:55 -0400

    migrated project to new local dir

---
+++ app/src/main/java/com/example/termproject/CreateAccountActivity.java
@@ -0,0 +1,22 @@
+package com.example.termproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.provider.BaseColumns;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+
+}
